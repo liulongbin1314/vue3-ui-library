7390e7102c4bd71be76017a4d63775c74b14a335
liulongbin <liulongbin1314@outlook.com>
2025-09-24 23:15:44 +0800

    针对Button组件进行按需导入和全局注册的测试

---
+++ examples/src/components/ButtonTest.vue
@@ -0,0 +1,10 @@
+<template>
+  <a-button></a-button>
+</template>
+
+<script setup>
+// 按需导入 Button 组件
+// import { AButton } from '../../../packages'
+</script>
+
+<style scoped></style>
--- examples/src/main.js
+++ examples/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// 导入组件库
+import AUi from '../../packages'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+// 安装组件库插件
+app.use(AUi)
+
+app.mount('#app')
