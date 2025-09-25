70f9cbda2d4f5aaa6f0a9d7416282d18ebfe1f46
liulongbin <liulongbin1314@outlook.com>
2025-09-26 00:31:29 +0800

    初始化theme主题包

--- examples/src/main.js
+++ examples/src/main.js
@@ -6,7 +6,7 @@ import AUi from '../../packages'
 // 全量导入主题包
 import '@ui-library/theme/src/index.scss'
 // 按需导入主题包
-// import '@ui-library/theme/src/button.scss'
+// import '@ui-library/components/button/src/style'
 
 const app = createApp(App)
 // 安装组件库插件
---
+++ packages/components/button/src/style/index.js
@@ -0,0 +1,2 @@
+// 导入 button 组件自己的样式表
+import '@ui-library/theme/src/button.scss'
