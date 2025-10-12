99afb38f1731f1875697733157b6350a10e5aaf1
liulongbin <liulongbin1314@outlook.com>
2025-10-12 22:39:50 +0800

    在其它按钮中覆写 button 组件的私有变量值

--- examples/src/components/ButtonTest.vue
+++ examples/src/components/ButtonTest.vue
@@ -2,7 +2,7 @@
   <p>基础按钮</p>
   <div class="row-gap">
     <a-button>默认</a-button>
-    <a-button type="priamry">主要</a-button>
+    <a-button type="primary">主要</a-button>
     <a-button type="success">成功</a-button>
     <a-button type="warning">警告</a-button>
     <a-button type="error">错误</a-button>
@@ -13,7 +13,7 @@
   <p>圆角按钮</p>
   <div class="row-gap">
     <a-button round>默认</a-button>
-    <a-button type="priamry" round>主要</a-button>
+    <a-button type="primary" round>主要</a-button>
     <a-button type="success" round>成功</a-button>
     <a-button type="warning" round>警告</a-button>
     <a-button type="error" round>错误</a-button>
@@ -22,7 +22,7 @@
   <p>禁用按钮</p>
   <div class="row-gap">
     <a-button disabled>默认</a-button>
-    <a-button type="priamry" disabled>主要</a-button>
+    <a-button type="primary" disabled>主要</a-button>
     <a-button type="success" disabled>成功</a-button>
     <a-button type="warning" disabled>警告</a-button>
     <a-button type="error" disabled>错误</a-button>
@@ -31,7 +31,7 @@
   <p>文字按钮</p>
   <div class="row-gap">
     <a-button text>默认</a-button>
-    <a-button type="priamry" text>主要</a-button>
+    <a-button type="primary" text>主要</a-button>
     <a-button type="success" text>成功</a-button>
     <a-button type="warning" text>警告</a-button>
     <a-button type="error" text>错误</a-button>
@@ -40,7 +40,7 @@
   <p>链接按钮</p>
   <div class="row-gap">
     <a-button link>默认</a-button>
-    <a-button type="priamry" link>主要</a-button>
+    <a-button type="primary" link>主要</a-button>
     <a-button type="success" link>成功</a-button>
     <a-button type="warning" link>警告</a-button>
     <a-button type="error" link>错误</a-button>
@@ -49,7 +49,7 @@
   <p>边框按钮</p>
   <div class="row-gap">
     <a-button border>默认</a-button>
-    <a-button type="priamry" border>主要</a-button>
+    <a-button type="primary" border>主要</a-button>
     <a-button type="success" border>成功</a-button>
     <a-button type="warning" border>警告</a-button>
     <a-button type="error" border>错误</a-button>
@@ -58,7 +58,7 @@
   <p>虚线边框按钮</p>
   <div class="row-gap">
     <a-button border dashed>默认</a-button>
-    <a-button type="priamry" border dashed>主要</a-button>
+    <a-button type="primary" border dashed>主要</a-button>
     <a-button type="success" border dashed>成功</a-button>
     <a-button type="warning" border dashed>警告</a-button>
     <a-button type="error" border dashed>错误</a-button>
@@ -67,7 +67,7 @@
   <p>块级按钮</p>
   <div class="col-gap">
     <a-button block>默认</a-button>
-    <a-button type="priamry" block>主要</a-button>
+    <a-button type="primary" block>主要</a-button>
     <a-button type="success" block>成功</a-button>
     <a-button type="warning" block>警告</a-button>
     <a-button type="error" block>错误</a-button>
@@ -76,7 +76,7 @@
   <p>按钮尺寸</p>
   <div class="row-gap">
     <a-button>默认</a-button>
-    <a-button type="priamry" size="default">主要</a-button>
+    <a-button type="primary" size="default">主要</a-button>
     <a-button type="success" size="large">成功</a-button>
     <a-button type="warning" size="small">警告</a-button>
     <a-button type="error" size="small">错误</a-button>
@@ -85,14 +85,14 @@
   <p>圆形按钮</p>
   <div class="row-gap">
     <a-button circle>默认</a-button>
-    <a-button type="priamry" size="default" circle>主要</a-button>
+    <a-button type="primary" size="default" circle>主要</a-button>
     <a-button type="success" size="large" circle>成功</a-button>
     <a-button type="warning" size="small" circle>警告</a-button>
     <a-button type="error" size="small" circle>错误</a-button>
   </div>
   <div class="row-gap">
     <a-button circle icon="icon-upload"></a-button>
-    <a-button type="priamry" size="default" circle icon="icon-customer-filter"></a-button>
+    <a-button type="primary" size="default" circle icon="icon-customer-filter"></a-button>
     <a-button type="success" size="large" circle icon="icon-upper-left-arrow"></a-button>
     <a-button type="warning" size="small" circle icon="icon-volume-fill"></a-button>
     <a-button type="error" size="small" circle icon="icon-zoom-out-fill"></a-button>
@@ -101,7 +101,7 @@
   <p>图标按钮</p>
   <div class="row-gap">
     <a-button icon="icon-upload" suffix-icon="icon-customer">默认</a-button>
-    <a-button type="priamry" size="default" suffix-icon="icon-customer">主要</a-button>
+    <a-button type="primary" size="default" suffix-icon="icon-customer">主要</a-button>
     <a-button type="success" size="large" icon="icon-assessed-badge">成功</a-button>
     <a-button type="warning" size="small" icon="icon-assessed-badge">警告</a-button>
     <a-button type="error" size="small" icon="icon-assessed-badge">错误</a-button>
@@ -110,7 +110,7 @@
   <p>loading 加载按钮 --- {{ loading }}</p>
   <div class="row-gap">
     <a-button :loading="loading" @click="changeLoading">默认</a-button>
-    <a-button type="priamry" size="default" :loading="loading" @click="changeLoading"
+    <a-button type="primary" size="default" :loading="loading" @click="changeLoading"
       >主要</a-button
     >
     <a-button type="success" size="large" :loading="loading" @click="changeLoading">成功</a-button>
@@ -121,7 +121,7 @@
   <p>自动触发 loading 加载</p>
   <div class="row-gap">
     <a-button :before-change="beforeChangeHandler">默认</a-button>
-    <a-button type="priamry" size="default" :before-change="beforeChangeHandler">主要</a-button>
+    <a-button type="primary" size="default" :before-change="beforeChangeHandler">主要</a-button>
     <a-button type="success" size="large" :before-change="beforeChangeHandler">成功</a-button>
     <a-button type="warning" size="small" :before-change="beforeChangeHandler">警告</a-button>
     <a-button type="error" size="small" :before-change="beforeChangeHandler">错误</a-button>
@@ -130,7 +130,7 @@
   <p>group 按钮组</p>
   <a-button-group>
     <a-button>默认</a-button>
-    <a-button type="priamry">主要</a-button>
+    <a-button type="primary">主要</a-button>
     <a-button type="success">成功</a-button>
     <a-button type="warning">警告</a-button>
     <a-button type="error">错误</a-button>
@@ -139,7 +139,7 @@
   <p>子组件继承父组件的属性</p>
   <a-button-group size="large">
     <a-button>默认</a-button>
-    <a-button type="priamry">主要</a-button>
+    <a-button type="primary">主要</a-button>
     <a-button type="success">成功</a-button>
     <a-button type="warning">警告</a-button>
     <a-button type="error">错误</a-button>
