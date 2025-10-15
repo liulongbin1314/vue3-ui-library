efb2d607d5e2ee0a1dc77bcd0f33c8ecf609cc2b
liulongbin <liulongbin1314@outlook.com>
2025-10-16 00:50:52 +0800

    为 Icon 组件提供 icon 属性

--- packages/components/icon/src/index.vue
+++ packages/components/icon/src/index.vue
@@ -1,6 +1,11 @@
 <template>
   <i :class="[ns.b()]" :style="[styledColor, styledFSize]">
-    <slot></slot>
+    <!-- 1. 通过插槽渲染图标 -->
+    <slot v-if="$slots.default"></slot>
+    <!-- 2. 通过 iconfont 渲染字体图标 -->
+    <span class="iconfont" :class="icon" v-if="_isIconFont"></span>
+    <!-- 3. 通过 svg 渲染图标 -->
+    <component :is="icon" v-else></component>
   </i>
 </template>
 
@@ -9,9 +14,10 @@ defineOptions({
   name: 'a-icon'
 })
 
-const { color, size } = defineProps({
+const { color, size, icon } = defineProps({
   color: String,
-  size: [String, Number]
+  size: [String, Number],
+  icon: [String, Object]
 })
 
 import { useNamespace, useStyle } from '@ui-library/hooks'
@@ -22,4 +28,6 @@ const uStyle = useStyle()
 
 const styledFSize = computed(() => uStyle.fontSize(size))
 const styledColor = computed(() => uStyle.color(color))
+
+const _isIconFont = computed(() => typeof icon === 'string')
 </script>
