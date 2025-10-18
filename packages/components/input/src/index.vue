15a8a3989626ba665b771bc2dee7a9d47eb6d465
liulongbin <liulongbin1314@outlook.com>
2025-10-19 01:59:47 +0800

    设置文本框focus时候的边框色

--- packages/components/input/src/index.vue
+++ packages/components/input/src/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <div :class="[ns.b()]">
+  <div :class="[ns.b(), ns.is('focus', _isFocus)]">
     <!-- 前置区域 -->
     <!-- xxx -->
     <div :class="[ns.e('wrapper')]">
       <!-- 前缀区域 -->
       <!-- yyy -->
-      <input type="text" placeholder="请输入内容" :class="[ns.e('inner')]" />
+      <input
+        type="text"
+        placeholder="请输入内容"
+        :class="[ns.e('inner')]"
+        @focus="focusHandler"
+        @blur="blurHandler"
+      />
       <!-- 后缀区域 -->
       <!-- yyy -->
     </div>
@@ -20,6 +26,15 @@ defineOptions({
 })
 
 import { useNamespace } from '@ui-library/hooks'
+import { ref } from 'vue'
 
 const ns = useNamespace('input')
+const _isFocus = ref(false)
+
+const focusHandler = (e) => {
+  _isFocus.value = true
+}
+const blurHandler = (e) => {
+  _isFocus.value = false
+}
 </script>
