4e4fc4736c8ccb5db73652f5944fb392f2960207
liulongbin <liulongbin1314@outlook.com>
2025-10-26 01:21:22 +0800

    初始化col组件

--- examples/src/components/GridTest.vue
+++ examples/src/components/GridTest.vue
@@ -1,6 +1,13 @@
 <template>
   <a-row>
-    <a-col>123</a-col>
+    <!-- a-col-值 -->
+    <!-- a-col-24 -->
+    <a-col :span="24">24</a-col>
+  </a-row>
+
+  <a-row>
+    <a-col :span="12">12</a-col>
+    <a-col :span="12">12</a-col>
   </a-row>
 </template>
 
--- packages/components/col/src/index.vue
+++ packages/components/col/src/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <component :is="tag" :class="[ns.b()]">
+  <component :is="tag" :class="[ns.b(), spanClass]">
     <slot></slot>
   </component>
 </template>
@@ -9,15 +9,25 @@ defineOptions({
   name: 'a-col'
 })
 
-defineProps({
+const props = defineProps({
   tag: {
     type: String,
     default: 'div'
+  },
+  span: {
+    type: Number,
+    default: 0
   }
 })
 
 import { useNamespace } from '@ui-library/hooks'
+import { computed } from 'vue'
+
 const ns = useNamespace('col')
+
+const spanClass = computed(() => {
+  return props.span ? `${ns.namespace}-col-${props.span}` : ''
+})
 </script>
 
 <style scoped></style>
