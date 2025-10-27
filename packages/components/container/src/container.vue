4115782c4a9ad9053df36217ad9889344e933617
liulongbin <liulongbin1314@outlook.com>
2025-10-28 01:27:34 +0800

    自动检测Container的布局方向

--- packages/components/container/src/container.vue
+++ packages/components/container/src/container.vue
@@ -13,15 +13,25 @@ const { direction } = defineProps({
   direction: {
     type: String,
     // 默认情况下，让子元素在横向上进行排列
-    default: 'horizontal'
+    // horizontal 横向排列
+    // vertical 纵向排列
+    default: ''
   }
 })
 
 import { useNamespace } from '@ui-library/hooks'
-import { computed } from 'vue'
+import { computed, useSlots } from 'vue'
 const ns = useNamespace('container')
+const slots = useSlots()
 
 const isVertical = computed(() => {
-  return direction === 'vertical'
+  // return direction === 'vertical'
+  if (direction === 'vertical') return true
+  if (direction === 'horizontal') return false
+
+  const result = slots
+    .default()
+    .some((item) => item.type.name === 'a-header' || item.type.name === 'a-footer')
+  return result
 })
 </script>
