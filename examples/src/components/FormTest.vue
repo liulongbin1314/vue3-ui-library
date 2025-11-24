b5473e922980765499640b7b9fceb36a826ca03b
liulongbin <liulongbin1314@outlook.com>
2025-11-25 00:22:58 +0800

    了解async-validator的基本用法

--- examples/src/components/FormTest.vue
+++ examples/src/components/FormTest.vue
@@ -14,4 +14,28 @@
 
 <script setup></script>
 
+<script>
+// import Schema from 'async-validator'
+
+// const rules = {
+//   name: [
+//     { type: 'string', required: true, message: '姓名不能为空' },
+//     { min: 2, max: 5, message: '姓名的长度必须是2-5之间' }
+//   ],
+//   phone: [
+//     { type: 'string', required: true, message: '手机号不能为空' },
+//     { pattern: /^1[3-9]\d{9}$/, message: '手机号格式不正确' }
+//   ]
+// }
+
+// const data = { name: 'zs', phone: '13800000001' }
+
+// const validator = new Schema(rules)
+
+// validator.validate(data, (errors, fields) => {
+//   console.log('errors: ', errors)
+//   console.log('fields: ', fields)
+// })
+</script>
+
 <style scoped></style>
