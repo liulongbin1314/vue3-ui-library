dfad55df5166499d668800e0388319bce2540725
liulongbin <liulongbin1314@outlook.com>
2025-12-08 11:35:02 +0800

    可穿透

--- examples/src/components/MaskTest.vue
+++ examples/src/components/MaskTest.vue
@@ -1,9 +1,23 @@
 <template>
   <p>基础用法</p>
-  <a-mask gray>123</a-mask>
-  <!-- <a-mask gray>456</a-mask> -->
+  <a-mask :gray="false" :blur="false" penetrable>
+    <div class="box">Hello world.</div>
+  </a-mask>
 </template>
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped>
+.box {
+  box-sizing: border-box;
+  width: 500px;
+  height: 300px;
+  border-radius: 15px;
+  padding: 20px;
+  background-color: red;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
--- packages/components/mask/src/index.js
+++ packages/components/mask/src/index.js
@@ -1,4 +1,4 @@
-import { h, defineComponent } from 'vue'
+import { h, defineComponent, computed } from 'vue'
 import { useNamespace, useZIndex } from '@ui-library/hooks'
 
 export default defineComponent({
@@ -13,6 +13,11 @@ export default defineComponent({
     blur: {
       type: Boolean,
       default: true
+    },
+    // 是否可穿透
+    penetrable: {
+      type: Boolean,
+      default: false
     }
   },
   emits: [],
@@ -23,13 +28,20 @@ export default defineComponent({
     const styledZIndex = { zIndex: currentZIndex.value }
     genNextIndex()
 
+    const isPenetrable = computed(() => props.penetrable && !props.blur && !props.gray)
+
     // 必须返回一个渲染函数
     return () => {
       // 根元素
       return h(
         'div',
         {
-          class: [ns.b(), ns.is('gray', props.gray), ns.is('blur', props.blur)],
+          class: [
+            ns.b(),
+            ns.is('gray', props.gray),
+            ns.is('blur', props.blur),
+            ns.is('penetrable', isPenetrable.value)
+          ],
           style: [styledZIndex]
         },
         [
