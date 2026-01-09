85cb53b46ce5ecd4a4e7111ce498d966bfb96ac9
liulongbin <liulongbin1314@outlook.com>
2026-01-10 01:40:53 +0800

    实现AForm组件的按需导入

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <RadioTest />
+  <FormTest />
 </template>
 
 <script setup>
@@ -9,8 +9,8 @@
 // import GridTest from './components/GridTest.vue'
 // import CheckboxTest from './components/CheckboxTest.vue'
 // import SwitchTest from './components/SwitchTest.vue'
-import RadioTest from './components/RadioTest.vue'
-// import FormTest from './components/FormTest.vue'
+// import RadioTest from './components/RadioTest.vue'
+import FormTest from './components/FormTest.vue'
 // import MessageTest from './components/MessageTest.vue'
 // import MaskTest from './components/MaskTest.vue'
 // import DialogTest from './components/DialogTest.vue'
