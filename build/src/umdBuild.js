bb3916e1a4ea4802f7ca7142071a77af931182e7
liulongbin <liulongbin1314@outlook.com>
2026-01-20 21:54:10 +0800

    实现svg的全量打包

--- build/src/umdBuild.js
+++ build/src/umdBuild.js
@@ -34,9 +34,41 @@ const umdBuildEntry = async (isMinify = false) => {
   })
 }
 
+// umd 全量打包 svg 图标的入口函数
+const umdSvgBuildEntry = async (isMinify = false) => {
+  // 创建打包器
+  const writeBundles = await rollup({
+    // 入口相关的配置
+    // 打包的入口模块
+    input: join(inputDir, 'icons/index.js'),
+    plugins: [vue(), nodeResolve(), esbuild({ minify: isMinify, sourceMap: true })],
+    external: ['vue']
+  })
+
+  // 调用打包器的 write 函数，进行打包
+  await writeBundles.write({
+    // 输出配置
+    // 打包的格式
+    format: 'umd',
+    // 输出的文件路径（需要包含文件名）
+    file: join(outputUmd, `icon.full${isMinify ? '.min' : ''}.js`),
+    // 组件库的全局变量名称
+    name: 'EscookIcon',
+    // 只使用具名导出
+    exports: 'named',
+    // 外部依赖
+    globals: { vue: 'Vue' }
+  })
+}
+
 // 今后，凡是需要放到 gulp.series 中的任务，必须是 async 异步函数，且必须 return Promise 对象
 export const buildUmd = async () => {
-  return Promise.all([umdBuildEntry(), umdBuildEntry(true)])
+  return Promise.all([
+    umdBuildEntry(),
+    umdBuildEntry(true),
+    umdSvgBuildEntry(),
+    umdSvgBuildEntry(true)
+  ])
 }
 
 // 测试 umd 打包
