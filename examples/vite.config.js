0546b09fe3d73be254d87ebad259b7ec47a3cebc
liulongbin <liulongbin1314@outlook.com>
2026-01-24 21:19:35 +0800

    动态加载指定组件的样式

--- examples/vite.config.js
+++ examples/vite.config.js
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 
+// 把大驼峰命名法的名字，转换成连字符的格式
+// AButton  ->  a-button
+// AButtonGroup  ->  a-button-group
+const toKebabCase = (name) => {
+  return name
+    .replace(/([A-Z]+)([A-Z])/g, '$1-$2')
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .toLowerCase()
+}
+
+console.log(toKebabCase('AButton'))
+
 // 实现 EscookUI 组件的自动导入
 // 形参中的 name 是组件的名字 AButton  ACheckboxGroup
 const EscookUIResolver = (mtype = 'es') => {
@@ -10,13 +22,16 @@ const EscookUIResolver = (mtype = 'es') => {
 
     // 如果组件的 name 名称以字母 A 开头，则从 escook-ui 中自动加载组件及样式
     if (name.startsWith('A')) {
+      const componentName = toKebabCase(name)
+      const dirname = componentName.slice(2)
+
       return {
         // 需要自动导入的组件的名字
         name,
         // 导入的路径（模块、包名）
         from: 'escook-ui',
         // 副作用
-        sideEffects: [`escook-ui/${mtype}/components/button/src/style/index.js`]
+        sideEffects: [`escook-ui/${mtype}/components/${dirname}/src/style/index.js`]
       }
     }
   }
