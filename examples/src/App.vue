3ab47acd7527719705c557175ec4cff364d3b865
liulongbin <liulongbin1314@outlook.com>
2026-01-26 11:03:43 +0800

    切换测试版本

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <DialogTest />
+  <ButtonTest />
 </template>
 
 <script setup>
-// import ButtonTest from './components/ButtonTest.vue'
+import ButtonTest from './components/ButtonTest.vue'
 // import IconTest from './components/IconTest.vue'
 // import InputTest from './components/InputTest.vue'
 // import GridTest from './components/GridTest.vue'
@@ -13,7 +13,7 @@
 // import FormTest from './components/FormTest.vue'
 // import MessageTest from './components/MessageTest.vue'
 // import MaskTest from './components/MaskTest.vue'
-import DialogTest from './components/DialogTest.vue'
+// import DialogTest from './components/DialogTest.vue'
 // import MessageBoxTest from './components/MessageBoxTest.vue'
 // import DrawerTest from './components/DrawerTest.vue'
 </script>
